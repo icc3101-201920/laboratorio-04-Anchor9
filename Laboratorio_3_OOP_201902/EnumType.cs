f4ba574a42a9bb253faf8d3ac50fc4bc4ddceb32
unknown <fjmiranda4iuandes.cl>
2019-08-30 10:09:53 -0400

    L4

---
+++ Laboratorio_3_OOP_201902/EnumType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_3_OOP_201902
+{
+    class EnumType
+
+      
+    {
+        public enum Enumtype
+
+        {
+            None,
+            melee,
+            range,
+            longRange,
+            buff,
+            buffmelee,
+            buffrange,
+            bufflongrange,
+            captain,
+            weather,
+        }
+
+    }
+}
